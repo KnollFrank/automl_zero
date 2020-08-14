e2773de48e9cbb0dace691d2bbe888bf538ffb35
Frank Knoll <Knoll_Frank@web.de>
2020-08-14 21:01:33 +0200

    making ExecutorTest.ReportsErrors pass again

--- executor_test.cc
+++ executor_test.cc
@@ -381,10 +381,10 @@ namespace automl_zero
         EXPECT_EQ(valid_errors.size(), num_valid_examples);
         EXPECT_THAT(
             train_errors,
-            ElementsAre(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0));
+            ElementsAre(0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0));
         EXPECT_THAT(
             valid_errors,
-            ElementsAre(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0));
+            ElementsAre(0.0, 1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0));
     }
 
     TEST(ExecutorTest, ItereatesThroughFeatures)
